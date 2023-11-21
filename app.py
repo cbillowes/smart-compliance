92bea034a6e91451d86f566ddd8056df0da91040
Clarice Bouwer <cbillowes@gmail.com>
2023-11-21 20:04:39 +0400

    wip: canvas image cropper

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from smart_compliance.kyc.core import KycPhoto, Kyc
 from smart_compliance.kyc.detector import verify_face
+from streamlit_drawable_canvas import st_canvas
+from PIL import Image
 import streamlit as st
 import numpy as np
 import pandas as pd
@@ -24,6 +26,39 @@ similarity_metrics = [
     "euclidean",
     "euclidean_l2"
 ]
+
+def throwaway():
+    drawing_mode = "rect"
+    stroke_width = 3
+    stroke_color = "#000"
+    bg_color = "transparent"
+    realtime_update = True
+    bg_image = st.file_uploader("Background image:", type=["png", "jpg"])
+
+    if bg_image:
+        height = Image.open(bg_image).size[1]
+        width = Image.open(bg_image).size[0]
+        canvas_result = st_canvas(
+            fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
+            stroke_width=stroke_width,
+            stroke_color=stroke_color,
+            background_color=bg_color,
+            background_image=Image.open(bg_image) if bg_image else None,
+            update_streamlit=realtime_update,
+            height=height,
+            width=width,
+            drawing_mode=drawing_mode,
+            point_display_radius=0,
+            key="canvas",
+        )
+
+        if canvas_result.image_data is not None:
+            st.image(canvas_result.image_data)
+        if canvas_result.json_data is not None:
+            objects = pd.json_normalize(canvas_result.json_data["objects"]) # need to convert obj to str because PyArrow
+            for col in objects.select_dtypes(include=['object']).columns:
+                objects[col] = objects[col].astype("str")
+            st.dataframe(objects)
 
 
 def models_form():
@@ -69,6 +104,7 @@ def selfie_form():
                 selfie.read(), np.uint8), cv2.IMREAD_UNCHANGED)
             kyc.register_selfie(
                 KycPhoto(image, scale_factor=scale_factor, padding=padding))
+
 
     with st.expander("Selfie"):
         col1, col2 = st.columns(2)
@@ -238,12 +274,13 @@ def main():
 
     st.sidebar.title("Compliance documents")
 
-    models_form()
-    similarity_metrics_form()
-    selfie_form()
-    document_form()
-    verification_form()
-    details_form()
+    throwaway()
+    # models_form()
+    # similarity_metrics_form()
+    # selfie_form()
+    # document_form()
+    # verification_form()
+    # details_form()
 
     st.sidebar.divider()
     st.sidebar.image("./hero.png", use_column_width=True,
