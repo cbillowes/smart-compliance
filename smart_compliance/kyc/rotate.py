d68129db246fe64b1c045c24a34aa0cf43c06600
Clarice Bouwer <cbillowes@gmail.com>
2023-11-25 16:49:06 +0400

    refactor rotate

--- smart_compliance/kyc/rotate.py
+++ smart_compliance/kyc/rotate.py
@@ -1,15 +1,19 @@
 import cv2
-import numpy as np
+from smart_compliance.kyc.detector import detect_faces
 
-def rotate(image, loop_while, angle):
+def rotate(image, angle):
     angle = angle if angle > 0 else 1
     rotation = 1  # avoid an infinite loop
-    while (loop_while(image) == True and rotation < 360):
-        (h, w) = image.shape[:2]
-        center = (w / 2, h / 2)
-        rotation += angle
-        M = cv2.getRotationMatrix2D(center, angle, 1.0)
-        image = cv2.warpAffine(image, M, (w, h))
+    while (len(detect_faces(image)) == 0 and rotation < 360):
+        try:
+            (h, w) = image.shape[:2]
+            center = (w / 2, h / 2)
+            rotation += angle
+            M = cv2.getRotationMatrix2D(center, angle, 1.0)
+            image = cv2.warpAffine(image, M, (w, h))
+        except Exception as e:
+            print("Could not rotate image: " + str(e))
+            break
     return image
 
 
