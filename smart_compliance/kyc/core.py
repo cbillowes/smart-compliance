4a8e3c2ae30ae711b1ea275110a34b97a938cb4d
Clarice Bouwer <cbillowes@gmail.com>
2023-11-25 16:48:36 +0400

    error handling on kyc core

--- smart_compliance/kyc/core.py
+++ smart_compliance/kyc/core.py
@@ -1,24 +1,44 @@
-import numpy as np
+import traceback
+import sys
 from smart_compliance.kyc.preparator import process
 from smart_compliance.kyc.detector import extract_faces_for_selfie, extract_faces_for_document
 
 
 class KycSelfie:
     def __init__(self, image) -> None:
-        self.original, self.processed = process(image)
-        faces, image_with_rectangles = extract_faces_for_selfie(self.processed)
-        self.detected_faces = image_with_rectangles
-        self.base_image = faces[0]
-        self.face = faces[1] if len(faces) > 1 else None
+        try:
+            self.original, self.processed = process(image)
+            faces, image_with_rectangles = extract_faces_for_selfie(self.processed)
+            self.detected_faces = image_with_rectangles
+            self.error = None
+
+            if len(faces) == 0:
+                self.error = "No faces detected. Please try a different image."
+            else:
+                self.base_image = faces[0]
+                self.face = faces[1] if len(faces) > 1 else None
+        except Exception as e:
+            print("Could not process selfie.")
+            print(e)
+            print(traceback.format_exc())
+            print(sys.exc_info()[2])
+            self.error = "The image is not valid. Please try a different image."
 
 
 class KycDocument:
     def __init__(self, image) -> None:
-        self.original, self.processed = process(image)
-        faces, image_with_rectangles = extract_faces_for_document(self.processed)
-        self.detected_faces = image_with_rectangles
-        self.base_image = faces[0]
-
+        try:
+            self.original, self.processed = process(image)
+            faces, image_with_rectangles = extract_faces_for_document(self.processed)
+            self.detected_faces = image_with_rectangles
+            self.error = None
+            if len(faces) == 0:
+                self.error = "No faces detected. Please try a different image."
+            else:
+                self.base_image = faces[0]
+        except Exception as e:
+            print("Could not process document: " + str(e))
+            self.error = "The image is not valid. Please try a different image."
 
 class Kyc:
     def __init__(self) -> None:
